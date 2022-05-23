73a41d807be16ea15313b3a14e5ca7b3ee269ccc
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2022-05-23 22:45:57 +0700

    1.3. математические операции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,23 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2);          //округление в сторону ближ.числа
+        System.out.println(1.0 / 2);        //вывод числа с плавающей точкой (хранятся иначе, чем простые)
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+
+        System.out.println("2" + "2");              //конкатенация = склеивание строк
+        System.out.println("2" + 2);                    //преобразования числа в строку
+        System.out.println(2 + "2");
+
+        System.out.println((2 + 2) * 2);
+
+        System.out.println("2 + 2 = " + (2 + 2));   //чтобы избежать склейки, брать вычисляемое выражение в скобки 
     }
 
 }
