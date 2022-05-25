5df1321326c85bcc5d605850aacdf7c4bc9ac15a
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2022-05-25 23:41:02 +0700

    1.11 Добавили библиотеку tetsng, через описание в конфиг.файле сборщика gradle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Triangle.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Triangle.java
@@ -5,14 +5,14 @@ public class Triangle {
     public double b;
     public double l;
 
-public Triangle(double a, double b, double l ){
+public Triangle(double a, double b, double l ){                             //конструктор
     this.a = a;
     this.b = b;
     this.l = l;
 
 }
 
-    public  double square() {
+    public  double square() {                                                   // метод
         return (((this.a * this.b)/2) * Math.sin(this.l));
     }
 }
